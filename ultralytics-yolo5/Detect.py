3c732f85f25bec352bcada92dbc209c69203b0e3
Muhammad Fauzan <mfauzan51087@gmail.com>
2022-10-23 18:19:30 +0700

    Menambhkan File Detect Video Baru

--- ultralytics-yolo5/Detect.py
+++ ultralytics-yolo5/Detect.py
@@ -1,38 +1,22 @@
 import os
 import torch
+import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
-model_name='best.pt'
-model = torch.hub.load(os.getcwd(), 'custom', source='local', path = model_name, force_reload = True)
-#print(os.getcwd())
-#model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
+
+model = torch.hub.load(os.getcwd(), 'custom', source='local', path = 'best.pt', force_reload = True)
 img = 'Valid/2.jpg'
-#cap = cv2.VideoCapture(0)
+
 results = model(img)
-print(type(results))
-print(results.xyxy[0])  # img1 predictions (tensor)
-print(results.pandas().xyxy[0])  # img1 predictions (pandas)
-#print(results.parametes)
-#print(np.squeeze(results.render()))
-#while True:
-#    _,frame = img.read()
-    
-'''
-while cap.isOpened():
-    ret, frame = cap.read()
-    print(type(frame),frame.shape)
-    
-    # Make detections 
-    results = model(frame)
-    
-    cv2.imshow('YOLO', np.squeeze(results.render()))
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
-#results = model(img)
-#print(results)
-'''
+import pandas as pd
+
+Xmin= results.pandas().xyxy[0]
+
+#print(Xmin)
+print(Xmin['name'].notna())
+#df = pd.DataFrame(Xmin)
+
+#print(results.pandas().xyxy[0])  # img1 predictions (pandas)
+
 #2
