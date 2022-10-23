768eb89b1db9e82378d3333c5c823c490d9ce6eb
Muhammad Fauzan <mfauzan51087@gmail.com>
2022-10-23 19:27:30 +0700

    Fix3

--- ultralytics-yolo5/Detect.py
+++ ultralytics-yolo5/Detect.py
@@ -1,3 +1,5 @@
+# image yang terdetct 2,4,5,8,13,14
+
 import os
 import torch
 from matplotlib import pyplot as plt
@@ -5,14 +7,20 @@ import numpy as np
 import cv2
 
 model = torch.hub.load(os.getcwd(), 'custom', source='local', path = 'best.pt', force_reload = True)
-img = 'Valid/19.jpg'
+img = 'Valid/4.jpg'
 gambar = cv2.imread(img)
 results = model(img)
 
 cls = results.pandas().xyxy[0]['name'].to_numpy()
 conf = results.pandas().xyxy[0]['confidence'].to_numpy()
+xmin = results.pandas().xyxy[0]['xmin'].to_numpy()
+ymin = results.pandas().xyxy[0]['ymin'].to_numpy()
+xmax = results.pandas().xyxy[0]['xmax'].to_numpy()
+ymax = results.pandas().xyxy[0]['ymax'].to_numpy()
+
 print(cls)
 print(conf)
+ 
+cv2.imshow('YOLO', np.squeeze(results.render()))
 
 
-#2,4,5,8,13,14
